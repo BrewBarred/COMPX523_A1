322e88d70d75a680efafe42bac7fd5a5c33c1a5c
BrewBarred <Elayjahh7@hotmail.com>
2026-05-07 16:35:52 +1200

    Finished test.py

--- test.py
+++ test.py
@@ -15,36 +15,52 @@ Optional, but recommended:
     git push -u origin main
 """
 
-"""
-Import the necessary libraries
-"""
-import capymoa
-# confirm capyMOA was successfully imported
-print(f'CapyMOA version {capymoa.__version__} detected.')
+""" Import the necessary libraries """
+# import the capymoa "Electricity" dataset
 from capymoa.datasets import Electricity
-# confirm Electricity dataset was successfully imported
-print(f'Loaded dataset: "{Electricity()}"')
-# confirm classifier can be imported from our custom file
-from assignment1_1631819 import KNN_cw_1631819 as classifier
-print(f'Detected classifier: "{classifier}"')
+# import our preferred classifier
+from assignment1_1631819 import KNN_cw_1631819 as clsf
 
-# run a test script to verify that CapyMOA is works
+""" Test to ensure capymoa and custom classifer works """
 stream = Electricity()
-schema
+print(f'Loaded stream: "{stream}"')
+schema = stream.get_schema()
+print(f'Fetched schema:\n{schema}')
+classifier = clsf(schema=schema, k=3, w=500)
+classifier_name = clsf.__name__
+print(f'\nDetected classifier: "{classifier_name}"')
+
+""" Setup the stream vars """
+correct = 0
 # store unique stream labels
 labels = set()
 # count instances
 i = 0
+
+""" Start training on/predicting datastream """
+print(f'Streaming, classifying and training on "{stream}" data-stream...')
 # iterate through the whole stream
 while stream.has_more_instances():
     # track instance count
     i+=1
+
     # fetch the next instance in the stream
     instance = stream.next_instance()
+    # use our nominated classifier to predict the label of this instnace
+    prediction = classifier.predict(instance)
+    # if the label is correct, add it to the prediciton count
+    if prediction == instance.y_index:
+        correct+=1
+    # train the classifier further
+    classifier.train(instance)
+
     # determine the label of this instance
     label = instance.y_index
     # add unique labels to our set (since it's a set, duplicates are automatically ignored)
     labels.add(label)
 
-# 4. Print the results
+""" Print the results """
 print(f'Found {len(labels)} unique labels {labels} in the "{stream}" data-stream.')
+print(f'{classifier_name} accuracy: {correct/i*100:.1f}% ({i} instances)')
+print(f'Class windows: {list(classifier.class_window.keys())}')
+print(f'Window sizes: {[classifier.class_window[k].size() for k in classifier.class_window]}')
